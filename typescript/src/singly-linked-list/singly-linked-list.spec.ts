98e4ce2e7ef852aa6c11a2089f13f7e2c483360d
André Pereira <andrepereira97@yahoo.com>
2023-04-27 18:24:42 -0300

    :sparkles: add singly linked list

---
+++ typescript/src/singly-linked-list/singly-linked-list.spec.ts
@@ -0,0 +1,189 @@
+import { describe, expect, it } from 'vitest';
+import { OutOfBoundsError, SinglyLinkedList } from './singly-linked-list';
+
+const generateEmptyList = (): SinglyLinkedList<string> =>
+  new SinglyLinkedList<string>();
+
+const generateNonEmptyList = (): SinglyLinkedList<string> => {
+  const list = new SinglyLinkedList<string>();
+  list.push('any_data1');
+  list.push('any_data2');
+  list.push('any_data3');
+  return list;
+};
+
+describe('SinglyLinkedList', () => {
+  describe('getByIndex', () => {
+    it('should get by index', () => {
+      const list = generateNonEmptyList();
+
+      expect(list.getByIndex(0)).toBe('any_data1');
+    });
+
+    it('should throw OutOfBoundsError if didnt pass a valid index', () => {
+      const list = generateNonEmptyList();
+
+      expect(() => list.getByIndex(-1)).toThrow(OutOfBoundsError);
+      expect(() => list.getByIndex(list.size)).toThrow(OutOfBoundsError);
+    });
+  });
+
+  describe('unshift', () => {
+    it('should add to front', () => {
+      const list1 = generateEmptyList();
+      list1.unshift('any_data');
+
+      expect(list1.getByIndex(0)).toBe('any_data');
+
+      const list2 = generateNonEmptyList();
+
+      list2.unshift('any_data');
+
+      expect(list2.getByIndex(0)).toBe('any_data');
+    });
+  });
+
+  describe('push', () => {
+    it('should add to back', () => {
+      const list1 = generateEmptyList();
+      list1.push('any_data');
+
+      expect(list1.getByIndex(list1.size - 1)).toBe('any_data');
+
+      const list2 = generateNonEmptyList();
+
+      list2.push('any_data');
+
+      expect(list2.getByIndex(list2.size - 1)).toBe('any_data');
+    });
+  });
+
+  describe('add', () => {
+    it('should add to index', () => {
+      const list1 = generateEmptyList();
+      list1.add(0, 'any_data');
+
+      expect(list1.getByIndex(0)).toBe('any_data');
+
+      const list2 = generateNonEmptyList();
+      list2.add(0, 'any_data');
+
+      expect(list2.getByIndex(0)).toBe('any_data');
+
+      const list3 = generateNonEmptyList();
+      list3.add(2, 'any_data');
+
+      expect(list3.getByIndex(2)).toBe('any_data');
+
+      const list4 = generateNonEmptyList();
+
+      list4.add(list4.size, 'any_data');
+
+      expect(list4.getByIndex(list4.size - 1)).toBe('any_data');
+    });
+
+    it('should throw OutOfBoundsError if didnt pass a valid index', () => {
+      const list = generateNonEmptyList();
+
+      expect(() => list.add(-1, 'any_data')).toThrow(OutOfBoundsError);
+
+      expect(() => list.add(list.size + 1, 'any_data')).toThrow(
+        OutOfBoundsError,
+      );
+    });
+  });
+
+  describe('shift', () => {
+    it('should remove from front', () => {
+      const list1 = generateEmptyList();
+
+      list1.shift();
+
+      expect(list1.getByIndex(0)).toBe(null);
+
+      const list2 = generateNonEmptyList();
+
+      list2.push('any_data');
+      list2.shift();
+
+      expect(list2.getByIndex(0)).not.toBe('any_data');
+    });
+  });
+
+  describe('pop', () => {
+    it('should remove from back', () => {
+      const list1 = generateEmptyList();
+
+      list1.pop();
+
+      expect(list1.getByIndex(list1.size - 1)).toBe(null);
+
+      const list2 = generateEmptyList();
+
+      list2.push('any_data1');
+
+      list2.pop();
+
+      expect(list2.getByIndex(list2.size - 1)).not.toBe('any_data1');
+
+      const list3 = generateNonEmptyList();
+
+      list3.push('any_data');
+      list3.pop();
+
+      expect(list3.getByIndex(list3.size - 1)).not.toBe('any_data');
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove from index', () => {
+      const list1 = generateEmptyList();
+
+      list1.remove(0);
+
+      expect(list1.getByIndex(0)).toBe(null);
+
+      const list2 = generateNonEmptyList();
+
+      list2.push('any_data');
+      list2.remove(0);
+
+      expect(list2.getByIndex(0)).not.toBe('any_data');
+
+      const list3 = generateNonEmptyList();
+
+      list3.push('any_data');
+
+      list3.remove(list3.size - 1);
+
+      expect(list3.getByIndex(list3.size - 1)).not.toBe('any_data');
+
+      const list4 = generateNonEmptyList();
+
+      list4.add(2, 'any_data');
+
+      list4.remove(2);
+
+      expect(list4.getByIndex(1)).not.toBe('any_data');
+    });
+
+    it('should throw OutOfBoundsError if didnt pass a valid index', () => {
+      const list = generateNonEmptyList();
+
+      expect(() => list.remove(-1)).toThrow(OutOfBoundsError);
+
+      expect(() => list.remove(list.size)).toThrow(OutOfBoundsError);
+    });
+  });
+
+  describe('toString', () => {
+    it('should print the list', () => {
+      const list = new SinglyLinkedList<number>();
+      list.push(1);
+      list.push(2);
+      list.push(3);
+
+      expect(list.toString()).toBe('1 2 3');
+    });
+  });
+});
---
+++ typescript/src/singly-linked-list/singly-linked-list.ts
@@ -0,0 +1,161 @@
+class LLNode<T> {
+  public data: T;
+  public next: LLNode<T> | null = null;
+
+  constructor(data: T) {
+    this.data = data;
+  }
+}
+
+export class OutOfBoundsError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'OutOfBoundsError';
+  }
+}
+
+export class SinglyLinkedList<T> {
+  public size = 0;
+  private head: LLNode<T> | null = null;
+
+  public getByIndex(index: number): T | null {
+    if (this.size === 0 || this.head === null) {
+      return null;
+    }
+
+    if (index < 0 || index >= this.size) {
+      throw new OutOfBoundsError('Index out of bounds');
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      if (current.next) {
+        current = current.next;
+      }
+    }
+
+    return current.data;
+  }
+
+  public unshift(data: T): void {
+    const node = new LLNode(data);
+    node.next = this.head;
+    this.head = node;
+    this.size++;
+  }
+
+  public push(data: T): void {
+    const node = new LLNode(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  public add(index: number, data: T): void {
+    if (index < 0 || index > this.size) {
+      throw new OutOfBoundsError('Index out of bounds');
+    }
+
+    if (index === 0 || this.head === null) {
+      this.unshift(data);
+      return;
+    }
+
+    if (index === this.size || this.head.next === null) {
+      this.push(data);
+      return;
+    }
+
+    const node = new LLNode(data);
+
+    let current = this.head;
+
+    for (let i = 0; i < index - 1; i++) {
+      if (current.next) {
+        current = current.next;
+      }
+    }
+
+    node.next = current.next;
+
+    current.next = node;
+
+    this.size++;
+  }
+
+  public shift(): void {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+    this.size--;
+  }
+
+  public pop(): void {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next?.next) {
+      current = current.next;
+    }
+    current.next = null;
+    this.size--;
+  }
+
+  public remove(index: number): void {
+    if (this.head === null) {
+      return;
+    }
+
+    if (index < 0 || index >= this.size) {
+      throw new OutOfBoundsError('Index out of bounds');
+    }
+
+    if (index === 0) {
+      this.shift();
+      return;
+    }
+
+    if (index === this.size - 1) {
+      this.pop();
+      return;
+    }
+
+    let current = this.head;
+
+    for (let i = 0; i < index - 1; i++) {
+      if (current.next) {
+        current = current.next;
+      }
+    }
+
+    if (current.next?.next) {
+      current.next = current.next.next;
+      this.size--;
+    }
+  }
+
+  public toString(): string {
+    let current = this.head;
+    let string = '';
+    while (current) {
+      string += current.data + ' ';
+      current = current.next;
+    }
+    return string.trim();
+  }
+}
