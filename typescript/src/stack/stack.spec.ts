f17c2f81ca1bab15f64f75f0d422a42e4aec5a30
André Pereira <andrepereira97@yahoo.com>
2023-04-28 19:18:30 -0300

    :white_check_mark: improve tests

--- typescript/src/stack/stack.spec.ts
+++ typescript/src/stack/stack.spec.ts
@@ -14,60 +14,80 @@ const generateNonEmptyStack = () => {
 describe('Stack', () => {
   it('should push an item', () => {
     const stack1 = generateEmptyStack();
+    expect(stack1.toString()).toBe('');
     stack1.push('any_data');
     expect(stack1.peek()).toBe('any_data');
     expect(stack1.size()).toBe(1);
     expect(stack1.isEmpty()).toBe(false);
+    expect(stack1.toString()).toBe('any_data');
 
     const stack2 = generateNonEmptyStack();
+    expect(stack2.toString()).toBe('any_data3,any_data2,any_data1');
     stack2.push('any_data');
     expect(stack2.peek()).toBe('any_data');
     expect(stack2.size()).toBe(4);
     expect(stack2.isEmpty()).toBe(false);
+    expect(stack2.toString()).toBe('any_data,any_data3,any_data2,any_data1');
   });
 
   it('should pop an item', () => {
     const stack1 = generateEmptyStack();
+    expect(stack1.toString()).toBe('');
     expect(() => stack1.pop()).not.toThrow();
     expect(stack1.pop()).toBeUndefined();
     expect(stack1.size()).toBe(0);
     expect(stack1.isEmpty()).toBe(true);
+    expect(stack1.toString()).toBe('');
 
     const stack2 = generateEmptyStack();
+    expect(stack2.toString()).toBe('');
     stack2.push('any_data');
+    expect(stack2.toString()).toBe('any_data');
     expect(stack2.pop()).toBe('any_data');
     expect(stack2.size()).toBe(0);
     expect(stack2.isEmpty()).toBe(true);
+    expect(stack2.toString()).toBe('');
 
     const stack3 = generateNonEmptyStack();
+    expect(stack3.toString()).toBe('any_data3,any_data2,any_data1');
     stack3.push('any_data');
+    expect(stack3.toString()).toBe('any_data,any_data3,any_data2,any_data1');
     expect(stack3.pop()).toBe('any_data');
     expect(stack3.size()).toBe(3);
     expect(stack3.isEmpty()).toBe(false);
+    expect(stack3.toString()).toBe('any_data3,any_data2,any_data1');
   });
 
   it('should peek an item', () => {
     const stack1 = generateEmptyStack();
+    expect(stack1.toString()).toBe('');
     expect(() => stack1.peek()).not.toThrow();
     expect(stack1.peek()).toBeUndefined();
     expect(stack1.size()).toBe(0);
     expect(stack1.isEmpty()).toBe(true);
+    expect(stack1.toString()).toBe('');
 
     const stack2 = generateNonEmptyStack();
+    expect(stack2.toString()).toBe('any_data3,any_data2,any_data1');
     expect(stack2.peek()).toBe('any_data3');
     expect(stack2.size()).toBe(3);
     expect(stack2.isEmpty()).toBe(false);
+    expect(stack2.toString()).toBe('any_data3,any_data2,any_data1');
   });
 
   it('should clear the stack', () => {
     const stack1 = generateEmptyStack();
+    expect(stack1.toString()).toBe('');
     stack1.clear();
     expect(stack1.size()).toBe(0);
     expect(stack1.isEmpty()).toBe(true);
+    expect(stack1.toString()).toBe('');
 
     const stack2 = generateNonEmptyStack();
+    expect(stack2.toString()).toBe('any_data3,any_data2,any_data1');
     stack2.clear();
     expect(stack2.size()).toBe(0);
     expect(stack2.isEmpty()).toBe(true);
+    expect(stack2.toString()).toBe('');
   });
 });
