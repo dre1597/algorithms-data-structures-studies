c65e90d87fb1b0bdda04ee94da083baf3e19617b
André Pereira <andrepereira97@yahoo.com>
2023-05-02 11:06:13 -0300

    :white_check_mark: improve tests

--- typescript/src/circular-queue/circular-queue.spec.ts
+++ typescript/src/circular-queue/circular-queue.spec.ts
@@ -14,69 +14,92 @@ const generateNonEmptyQueue = () => {
 describe('CircularQueue', () => {
   it('should enqueue an item', () => {
     const queue1 = generateEmptyQueue();
+    expect(queue1.toString()).toBe('');
     queue1.enqueue('any_data');
     expect(queue1.peek()).toBe('any_data');
     expect(queue1.size()).toBe(1);
     expect(queue1.isEmpty()).toBe(false);
+    expect(queue1.toString()).toBe('any_data');
 
     const queue2 = generateEmptyQueue();
+    expect(queue2.toString()).toBe('');
     queue2.enqueue('any_data');
+    expect(queue2.toString()).toBe('any_data');
     queue2.dequeue();
+    expect(queue2.toString()).toBe('');
     queue2.enqueue('any_data');
 
     expect(queue2.peek()).toBe('any_data');
     expect(queue2.size()).toBe(1);
     expect(queue2.isEmpty()).toBe(false);
+    expect(queue2.toString()).toBe('any_data');
 
     const queue3 = generateNonEmptyQueue();
+    expect(queue3.toString()).toBe('any_data1 any_data2 any_data3');
     queue3.enqueue('any_data');
     expect(queue3.peek()).toBe('any_data1');
     expect(queue3.size()).toBe(4);
     expect(queue3.isEmpty()).toBe(false);
+    expect(queue3.toString()).toBe('any_data1 any_data2 any_data3 any_data');
   });
 
   it('should dequeue an item', () => {
     const queue1 = generateEmptyQueue();
+    expect(queue1.toString()).toBe('');
     expect(() => queue1.dequeue()).not.toThrow();
     expect(queue1.dequeue()).toBeNull();
     expect(queue1.size()).toBe(0);
     expect(queue1.isEmpty()).toBe(true);
+    expect(queue1.toString()).toBe('');
 
     const queue2 = generateEmptyQueue();
+    expect(queue2.toString()).toBe('');
     queue2.enqueue('any_data');
+    expect(queue2.toString()).toBe('any_data');
     expect(queue2.dequeue()).toBe('any_data');
     expect(queue2.size()).toBe(0);
     expect(queue2.isEmpty()).toBe(true);
+    expect(queue2.toString()).toBe('');
 
     const queue3 = generateNonEmptyQueue();
+    expect(queue3.toString()).toBe('any_data1 any_data2 any_data3');
     expect(queue3.dequeue()).toBe('any_data1');
     expect(queue3.size()).toBe(2);
     expect(queue3.isEmpty()).toBe(false);
+    expect(queue3.toString()).toBe('any_data2 any_data3');
   });
 
   it('should peek an item', () => {
     const queue1 = generateEmptyQueue();
+    expect(queue1.toString()).toBe('');
     expect(() => queue1.peek()).not.toThrow();
     expect(queue1.peek()).toBeNull();
     expect(queue1.size()).toBe(0);
     expect(queue1.isEmpty()).toBe(true);
+    expect(queue1.toString()).toBe('');
 
     const queue2 = generateNonEmptyQueue();
+    expect(queue2.toString()).toBe('any_data1 any_data2 any_data3');
     expect(queue2.peek()).toBe('any_data1');
     expect(queue2.size()).toBe(3);
     expect(queue2.isEmpty()).toBe(false);
+    expect(queue2.toString()).toBe('any_data1 any_data2 any_data3');
   });
 
   it('should clear the queue', () => {
     const queue1 = generateEmptyQueue();
+    expect(queue1.toString()).toBe('');
     expect(() => queue1.clear()).not.toThrow();
     expect(queue1.size()).toBe(0);
     expect(queue1.isEmpty()).toBe(true);
+    expect(queue1.toString()).toBe('');
 
     const queue2 = generateNonEmptyQueue();
+    expect(queue2.toString()).toBe('any_data1 any_data2 any_data3');
     queue2.clear();
     expect(queue2.size()).toBe(0);
     expect(queue2.isEmpty()).toBe(true);
+    expect(queue2.toString()).toBe('');
   });
 
   it('should return a string versions of the queue', () => {
