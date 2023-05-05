693bedb813d8cca639826026f66518b2bea03b30
André Pereira <andrepereira97@yahoo.com>
2023-05-05 10:39:16 -0300

    :sparkles: add selectionsort

---
+++ typescript/src/algorithms/sort/selectionsort/selectionsort.ts
@@ -0,0 +1,17 @@
+export function selectionsort(array: number[]): number[] {
+  const length = array.length;
+
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[i]) {
+        swap(array, i, j);
+      }
+    }
+  }
+
+  return array;
+}
+
+function swap(array: number[], i: number, j: number) {
+  [array[i], array[j]] = [array[j], array[i]];
+}
