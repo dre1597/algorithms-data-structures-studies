b1d0446bbdf70694cc52ee94fe723914bfd4bc8f
André Pereira <andrepereira97@yahoo.com>
2023-05-05 10:08:08 -0300

    :sparkles: add bubblesort

---
+++ typescript/src/algorithms/sort/bubblesort/bubblesort.ts
@@ -0,0 +1,32 @@
+export function bubblesort1(array: number[]): number[] {
+  const length = array.length;
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (array[j + 1] < array[j]) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+
+  return array;
+}
+
+export function bubblesort2(array: number[]): number[] {
+  const length = array.length;
+
+  for (let i = 0; i < length - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < length - i - 1; j++) {
+      if (array[j + 1] < array[j]) {
+        swap(array, j, j + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return array;
+}
+
+function swap(array: number[], i: number, j: number) {
+  [array[i], array[j]] = [array[j], array[i]];
+}
